b983a56cb289b508a118f57e73a6f266c80d6e20
wolnikowa <78367914+wolnikowa@users.noreply.github.com>
2021-03-21 22:54:36 +0100

    Basic layout

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './header.scss';
 import jitLogo from "./../../assets/logoBiale.png";
+import jitName from "./../../assets/name.png";
 
 
 class Header extends React.Component{
@@ -9,7 +10,11 @@ class Header extends React.Component{
             <div className='header'>
                 <div className='images'>
                     <img src={jitLogo} />
-                    <img src="" />
+                    <div>
+                        <span>D</span>
+                        <span className='yellow'>o</span>
+                        <span>nuts</span>
+                    </div>
                 </div>
             </div>
         )
