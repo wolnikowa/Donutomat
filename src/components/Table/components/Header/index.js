63aadcdb18ab6145840d2e4755c7b7e368782809
Julia Bessman <julia.bessman@autonomik.pl>
2021-04-21 18:33:34 +0200

    Revert "Merge branch 'origin/Branch-Julii' into main"
    This reverts commit 6b3f78718467319ff9a1954e0225d1065463d08e, reversing
    changes made to ce6a3249fcda7d01f4679ebcb4f07def1401e279.

--- src/components/Table/components/Header/index.js
+++ src/components/Table/components/Header/index.js
@@ -1,28 +1,19 @@
-import './header.scss';
+
 import React from 'react';
 
-
-const Header = ({columns}) => {
+const Header = () => {
     return (
-<thead className="thead">
-              
-                           
-        <tr>
-        {
-            columns.map((column) => (
-            <th>{column.label}</th>             
-        ))
-        }
-                      
-        </tr>   
-                          
-  
 
+<thead>
+              
+<tr >
+    <th>Name</th>
+    <th>Added by</th> 
+    <th> Date of adding</th> 
+    <th>Donuts date</th>
+</tr>
 
 </thead>
 
     )}
 export default Header
-
-
-
