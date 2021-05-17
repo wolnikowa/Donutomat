b5f8be5f85afe07e33b7e8a258d06ae31962fc77
wolnikowa <78367914+wolnikowa@users.noreply.github.com>
2021-05-17 10:15:37 +0200

    Adding new donutor.

--- src/Pages/Add/index.js
+++ src/Pages/Add/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React,{useCallback} from 'react';
 import DonutForm from '../../components/DonutForm';
-
+const axios=require('axios')
 const Add = () => {
+    const onSubmit = useCallback((form) => {
+        axios.post('/api/donutors',{
+            id:form.id,
+            name:form.donutorName,
+            dateadd:new Date(form.donutDate).toISOString(),
+            datedonut:new Date(new Date(form.donutDate).getDate()+7).toISOString(), //?
+            addedby: "Ryszard Jakielski"
+        }
+        ).then((response)=> console.log(response))
+        .then(()=>window.location.replace("/"))
+        .catch((err)=>console.log(new Error(err)))
+    }, []);
+
     return (
-        <DonutForm />
+        <DonutForm onSubmit={onSubmit} />
     )
 }
 
